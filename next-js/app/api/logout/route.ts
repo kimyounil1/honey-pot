3c94d4c27d9d81b6b959ac71972461e666736ef9
lghgthb <lgh2151@gmail.com>
2025-08-21 06:27:18 +0000

    0821-1527

--- next-js/app/api/logout/route.ts
+++ next-js/app/api/logout/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 export async function GET() {
   // 응답 객체를 먼저 생성합니다.
   const baseUrl = process.env.BASE_URL
-  // const response = NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_BASE_URL || "http://54.252.129.84:3000"));
   const response = NextResponse.redirect(new URL("/", baseUrl));
 
   // 쿠키를 삭제합니다. (maxAge: 0)
